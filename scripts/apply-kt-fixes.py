35f013917e5cccf723a4703dd58ebaf840c8dd66
Zebedee Nicholls <zebedee.nicholls@climate-resource.com>
2025-04-22 18:50:21 +0200

    Apply Karl's fixes

--- scripts/apply-kt-fixes.py
+++ scripts/apply-kt-fixes.py
@@ -1,88 +1,55 @@
-import copy
 import json
 from pathlib import Path
 
 import pandas as pd
+from cmip_branded_variable_mapper import map_to_cmip_branded_variable
 
 
-def split_dimensions(d: str) -> list[str]:
+def split_dimensions(d: str) -> tuple[str, ...]:
     if "," in d:
-        res = [v.strip() for v in d.split(",")]
+        res = tuple([v.strip() for v in d.split(",")])
     else:
         # Assume space separated
-        res = [v.strip() for v in d.split(" ")]
+        res = tuple([v.strip() for v in d.split(" ")])
 
     return res
 
 
 def main():
     REPO_ROOT = Path(__file__).parents[1]
-    IN_DIR = REPO_ROOT / "variables"
+    OUT_DIR = REPO_ROOT / "branded-variables"
     KARL_FILE = "v1_2-Variables-Spreadsheet-KT-review-041525.xlsx"
 
-    karl_table = pd.read_excel(KARL_FILE, sheet_name="v1.2 Variables").set_index(
-        "Physical Parameter"
-    )
+    karl_table = pd.read_excel(KARL_FILE, sheet_name="v1.2 Variables")
 
-    for json_file in IN_DIR.glob("*.json"):
-        with open(json_file) as fh:
-            raw = json.load(fh)
+    for _, row in karl_table.iterrows():
+        if pd.isnull(row["Physical Parameter"]):
+            continue
 
-        updated = copy.deepcopy(raw)
-        variableRootDD = raw["variableRootDD"]
+        dimensions = split_dimensions(row["dimensions for determining branding suffix"])
+        cell_methods = row["cell_methods for determining branding suffix"].strip()
+        variable_root = row["variableRootDD"]
+        # Descriptions change no matter what I do, whatever
+        # description = row["Title"]
+        description = row["Description"]
 
-        karl_row = karl_table.loc[variableRootDD]
-        if len(karl_row.shape) > 1:
-            ket_comments = karl_row["KET comments"]
-            can_be_eliminated = ket_comments.map(
-                lambda x: not isinstance(x, float) and "see ***" in x
-            )
-            if (~can_be_eliminated).sum() < 1:
-                # All can be eliminated, so just take the first
-                karl_row = karl_row.iloc[0, :]
-
-            elif (~can_be_eliminated).sum() > 1:
-                print(f"Not clear how to deal with duplicates for {variableRootDD}")
-                continue
-
-            else:
-                karl_row = karl_row[~can_be_eliminated].loc[variableRootDD]
-
-        for karl_key, data_key, pre_processor in (
-            (
-                "cell_methods for determining branding suffix",
-                "cell_methods",
-                lambda x: x.strip(),
+        res = {
+            "cell_methods": cell_methods,
+            "dimensions": dimensions,
+            "variableRootDD": variable_root,
+            "description": description,
+            "@context": "TBD",
+            "id": "TBD",
+            "type": ["TBD"],
+            "label": "TBD",
+            "branded_variable": map_to_cmip_branded_variable(
+                variable_name=variable_root,
+                dimensions=dimensions,
+                cell_methods=cell_methods,
             ),
-            (
-                "dimensions for determining branding suffix",
-                "dimensions",
-                split_dimensions,
-            ),
-        ):
-            if len(karl_row.shape) > 1:
-                karl_value_l = karl_row.loc[:, karl_key].unique().tolist()
-                if len(karl_value_l) > 1:
-                    breakpoint()
-
-                karl_value = karl_value_l[0]
-
-            else:
-                karl_value = karl_row.loc[karl_key]
-
-            if pre_processor is not None:
-                karl_value = pre_processor(karl_value)
-
-            if data_key == "dimensions" and any("area" in v for v in karl_value):
-                print(
-                    "Assuming Karl didn't mean to put area in dimensions, but good to check. "
-                    f"{data_key=} {karl_value=}"
-                )
-
-            updated[data_key] = karl_value
-
-        with open(json_file, "w") as fh:
-            json.dump(updated, fh, indent=2)
+        }
+        with open(OUT_DIR / f"{res['branded_variable']}.json", "w") as fh:
+            json.dump(res, fh, indent=2, sort_keys=2)
 
 
 if __name__ == "__main__":
